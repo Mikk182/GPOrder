41568ab9f85edcd567cb29a0bc7f61fa47c3daf9
Mikk182 <mickael.chemin@gmail.com>
2017-03-15 11:11:46 +0100

    Ajout LoginName

--- GPOrder/Models/AccountViewModels.cs
+++ GPOrder/Models/AccountViewModels.cs
@@ -65,6 +65,11 @@ namespace GPOrder.Models
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
