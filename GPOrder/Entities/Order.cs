5b29f6d90ec0a9894e986eb5e3c89cf5222433b8
Mikk182 <mickael.chemin@gmail.com>
2017-03-30 17:34:53 +0200

    Fix Prix >= 0 & Description d'une ligne required

--- GPOrder/Entities/Order.cs
+++ GPOrder/Entities/Order.cs
@@ -12,17 +12,17 @@ namespace GPOrder.Models
         public Guid Id { get; set; }
         [DataType(DataType.Date)]
         public DateTime CreationDate { get; set; }
-        
+
         public string CreateUser_Id { get; set; }
         public virtual ApplicationUser CreateUser { get; set; }
-        
+
         public string DeliveryBoy_Id { get; set; }
         public virtual ApplicationUser DeliveryBoy { get; set; }
-        
+
         [DataType(DataType.Date)]
         public DateTime? LimitDate { get; set; }
         public bool IsLocked { get; set; }
-        
+
         public Guid? LinkedShop_Id { get; set; }
         public virtual Shop LinkedShop { get; set; }
 
@@ -44,7 +44,9 @@ namespace GPOrder.Models
         public DateTime OrderDate { get; set; }
         public bool IsLocked { get; set; }
         [Required]
+        [Range(0, double.MaxValue)]
         public decimal EstimatedPrice { get; set; }
+        [Range(0, double.MaxValue)]
         public decimal? RealPrice { get; set; }
 
         public virtual ICollection<OrderLine> OrderLines { get; set; }
@@ -52,13 +54,14 @@ namespace GPOrder.Models
         public Guid GroupedOrder_Id { get; set; }
         public virtual GroupedOrder GroupedOrder { get; set; }
     }
-    
+
     public class OrderLine
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public Guid Id { get; set; }
-        [StringLength(100, MinimumLength = 3)]
+
+        [Required]
         public string Description { get; set; }
 
         public Guid Order_Id { get; set; }
