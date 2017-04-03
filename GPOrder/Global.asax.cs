c0df5dd6c60e8887c44e5d6dc245e482f4ef8ce9
glanout <r.auvray@gmail.com>
2017-04-03 22:45:38 +0200

    inject user UICulture in CurrentThread

--- GPOrder/Global.asax.cs
+++ GPOrder/Global.asax.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Globalization;
+using System.Security.Claims;
+using System.Threading;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using GPOrder.Helpers;
 
 namespace GPOrder
 {
@@ -12,11 +12,32 @@ namespace GPOrder
     {
         protected void Application_Start()
         {
-            
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ControllerBuilder.Current.SetControllerFactory(typeof(LocalizationControllerFactory));
+        }
+
+        /// <summary>
+        /// seul moyen que j'ai trouvé pour injecter la culture de l'utilisateur dans le context...
+        /// </summary>
+        public class LocalizationControllerFactory : DefaultControllerFactory
+        {
+            public override IController CreateController(RequestContext requestContext, string controllerName)
+            {
+                var controller = base.CreateController(requestContext, controllerName);
+
+                var principal = requestContext.HttpContext.User.Identity as ClaimsIdentity;
+                if (principal != null && principal.IsAuthenticated)
+                {
+                    var userCulture = CultureInfo.GetCultureInfo(principal.GetUiCulture());
+                    Thread.CurrentThread.CurrentUICulture = userCulture;
+                }
+
+                return controller;
+            }
         }
     }
 }
