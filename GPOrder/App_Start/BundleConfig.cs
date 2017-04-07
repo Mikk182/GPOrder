65c8494f12b1067d98a4dfdef03d34d65aec87b4
glanout <r.auvray@gmail.com>
2017-04-07 10:15:44 +0200

    globalize!

--- GPOrder/App_Start/BundleConfig.cs
+++ GPOrder/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Collections.Generic;
+using System.Web;
 using System.Web.Optimization;
 
 namespace GPOrder
@@ -9,45 +10,72 @@ namespace GPOrder
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/globalize").Include(
+                "~/Scripts/cldr.js",
+                "~/Scripts/cldr/event.js",
+                "~/Scripts/cldr/supplemental.js",
+                "~/Scripts/cldr/unresolved.js",
+                "~/Scripts/globalize.js",
+                "~/Scripts/globalize/currency.js",
+                "~/Scripts/globalize/date.js",
+                "~/Scripts/globalize/message.js",
+                "~/Scripts/globalize/number.js",
+                "~/Scripts/globalize/plural.js",
+                "~/Scripts/globalize/relative-time.js",
+                "~/Scripts/globalize/unit.js"));
+
+            var bundle = new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.validate.js",
+                "~/Scripts/jquery.validate.unobtrusive.js",
+                "~/Scripts/jquery.validate.globalize.js");
+            bundle.Orderer = new AsIsBundleOrderer();
+            bundles.Add(bundle);
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryajax").Include(
-                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+                "~/Scripts/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            "~/Scripts/jquery-ui-{version}.js",
-            "~/Scripts/jquery-ui.unobtrusive-{version}.js"));
+                "~/Scripts/jquery-ui-{version}.js",
+                "~/Scripts/jquery-ui.unobtrusive-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/iln_datetimepicker").Include(
-                      "~/Scripts/moment-with-locales.min.js"
-                      ,"~/Scripts/bootstrap-datetimepicker.js"
-                      ,"~/Scripts/iln/datetimepicker.js"
-                      ));
+                "~/Scripts/moment-with-locales.js",
+                "~/Scripts/bootstrap-datetimepicker.js",
+                "~/Scripts/iln/datetimepicker.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/bootstrap.css",
+                "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                "~/Content/themes/base/jquery.ui.core.css",
+                "~/Content/themes/base/jquery.ui.datepicker.css",
+                "~/Content/themes/base/jquery.ui.theme.css"));
 
             bundles.Add(new StyleBundle("~/bundles/iln_datetimepicker_css").Include(
-                      "~/Content/bootstrap-datetimepicker.min.css"
-                      ));
+                "~/Content/bootstrap-datetimepicker.min.css"));
+        }
+    }
+
+    /// <summary>
+    /// permet à ce que les js soit référencés dans l'ordre qu'il sont mis dans le bundle
+    /// </summary>
+    internal class AsIsBundleOrderer : IBundleOrderer
+    {
+        public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+        {
+            return files;
         }
     }
 }
